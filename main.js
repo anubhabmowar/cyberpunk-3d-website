6392b390c80bde390e3761f0041bc650b2f18027
anubhabmowar <anubhabmowar@gmail.com>
2024-11-28 13:12:20 +0530

    adding project files

---
+++ main.js
@@ -0,0 +1,97 @@
+import "./style.css";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass";
+import { RGBShiftShader } from "three/examples/jsm/shaders/RGBShiftShader";
+import gsap from "gsap";
+
+
+// scene
+const scene = new THREE.Scene();
+// camera
+const camera = new THREE.PerspectiveCamera(
+  40,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.position.z = 3.5;
+
+// hdri
+new RGBELoader()
+  .load("https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/pond_bridge_night_1k.hdr", function (texture) {
+    const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+    scene.environment = envMap;
+    texture.dispose();
+    pmremGenerator.dispose();
+  });
+
+// GLTF
+const loader = new GLTFLoader();
+let model;
+loader.load(
+  "/DamagedHelmet.gltf",
+  function (gltf) {
+    model = gltf.scene;
+    scene.add(model);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+window.addEventListener("mousemove",(e)=>{
+  if(model){
+    const rotationX = (e.clientX/window.innerWidth-.5)*Math.PI*0.12;
+    const rotationY = (e.clientY/window.innerHeight-.5)*Math.PI*0.12;
+    gsap.to(model.rotation, {
+      x: rotationY,
+      y: rotationX,
+      duration: 0.9,
+      ease: "power2.out"
+    });
+  }
+});
+// event listeners to handle window resize
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  composer.setSize(window.innerWidth, window.innerHeight);
+});
+// renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#canvas"),
+  antialias: true,
+  alpha: true,
+});
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+const pmremGenerator = new THREE.PMREMGenerator(renderer);
+pmremGenerator.compileEquirectangularShader();
+
+// postprocessing
+const composer = new EffectComposer(renderer);
+composer.addPass(new RenderPass(scene, camera));
+
+const rgbShiftPass = new ShaderPass(RGBShiftShader);
+rgbShiftPass.uniforms["amount"].value = 0.0030; // adjust the amount as needed
+composer.addPass(rgbShiftPass);
+
+// controls
+
+
+// render
+function animate() {
+  window.requestAnimationFrame(animate);
+  composer.render();
+}
+animate();
+
